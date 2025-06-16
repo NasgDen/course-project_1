d1a3970201b1ddb22d45ba5d194545da949f546c
Denis Safonov <denissafonov_85@mail.ru>
2025-06-16 20:31:33 +0500

    Модули проверены линтерами

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 
 from src.utils import read_xlsx_file
 
-
 PATH_TO_XLSX_FILE = os.path.join(os.getcwd(), "data", "operations.xlsx")
+
+
 def main():
     transactions = read_xlsx_file(PATH_TO_XLSX_FILE)
     print(transactions)
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
 
-def read_xlsx_file(path):
+def read_xlsx_file(path: str) -> list[dict]:
     try:
         transactions = pd.read_excel(path)
         return transactions.to_dict("records")
     except FileNotFoundError:
         return []
-
