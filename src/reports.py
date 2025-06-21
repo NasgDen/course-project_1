3820e60e6a9a8c057793e0e8c0dc1037cba24cee
Denis Safonov <denissafonov_85@mail.ru>
2025-06-21 13:58:29 +0500

    В модуль reports.py добавлен декоратор json_decorator_with_filename

--- src/reports.py
+++ src/reports.py
@@ -1,7 +1,19 @@
 import datetime
 import pandas as pd
 from functools import wraps
-import json
+
+def json_decorator_with_filename(filename):
+    def decorator_func(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            print(function.__name__)
+            result = function(*args, **kwargs)
+            with open(filename, mode="w", encoding="utf-8") as file:
+                print(file)
+                result.to_json(file, orient='records', indent=4, force_ascii=False)
+            return result
+        return wrapper
+    return decorator_func
 
 
 def json_decorator(function):
@@ -9,15 +21,13 @@ def json_decorator(function):
     def wrapper(*args, **kwargs):
         print(function.__name__)
         result = function(*args, **kwargs)
-        df_to_json = result.to_json(orient='records', indent=4)
-        print(df_to_json)
         with open(f"{function.__name__}.json", mode="w", encoding="utf-8") as file:
             print(file)
             result.to_json(file, orient='records', indent=4, force_ascii=False)
         return result
     return wrapper
 
-@json_decorator
+@json_decorator_with_filename("reports.json")
 def spending_by_category(transactions, cateroty: str, data: str=""):
     if data:
         date_start = datetime.datetime.strptime(data, "%d.%m.%Y")
